666d9b38e961f557ef87f3a9f63b8a061d75016b
Troy McCartt <ausinmccartt@gmail.com>
2024-03-08 11:29:09 -0600

    Updated teh tables names

--- readFile.py
+++ readFile.py
@@ -4,29 +4,23 @@ import sqlite3
 conn = sqlite3.connect('quiz_bowl.db')
 cursor = conn.cursor()
 
+
+
 # Function to retrieve table names
 def get_table_names():
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     table_names = cursor.fetchall()
     return [name[0] for name in table_names]
+print("Table Names:",get_table_names(),"\n\n")
 
 # Function to retrieve all data from a table
 def get_table_data(table_name):
     cursor.execute(f"SELECT * FROM {table_name}")
     table_data = cursor.fetchall()
-    return table_data
+    print(table_name,table_data)
+get_table_data("Business_Ethics")
+get_table_data("Business_Data_MGMT")
+get_table_data("Business_Communication")
+get_table_data("Principles_of_Macro")
+get_table_data("Business_App_Development")
 
-# Get and display table names
-print("List of Table Names:")
-for name in get_table_names():
-    print(name)
-
-# Get and display data from each table
-for table_name in get_table_names():
-    print(f"\nData from Table: {table_name}")
-    table_data = get_table_data(table_name)
-    for row in table_data:
-        print(row)
-
-# Close connection
-conn.close()
